647d3e64ec523b25cdfa8e52b1f821e36c0afca9
dotcomboom <dotcomboom@users.noreply.github.com>
2019-02-04 18:33:25 -0600

    Now I know why gg me

--- examples/tests_client_tls.py
+++ examples/tests_client_tls.py
@@ -1,5 +1,12 @@
 import pituophis
 
+#    ____
+#   /    \      Here rests my sanity, since *each* time I tested TLS
+#  |2019  |     I wrote "khaze.net" instead of "khzae.net".
+#  | -2019|     This goes to the multitude of minutes spent trying to debug
+#  |      |     TLS, when it was just fine all along and it was just
+# ~~~~~~~~~~    me misremembering the domain name. I apologize. Nice gopherhole btw
+
 req = pituophis.Request()
 req.host = input()
 req.port = 105
