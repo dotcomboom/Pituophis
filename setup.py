2d90e365ce3beb7f98a0e135695ab5f83593e53f
dotcomboom <dotcomboom@users.noreply.github.com>
2019-02-12 22:43:31 -0600

    0.91

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='Pituophis',
-    version='0.90',
+    version='0.91',
     packages=['pituophis'],
     url='https://github.com/dotcomboom/Pituophis',
     license='BSD 2-Clause License',
