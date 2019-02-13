c583824cd4a33933cababb57f9a27aeb51124824
dotcomboom <dotcomboom@users.noreply.github.com>
2019-02-12 20:29:19 -0600

    setup.py, we at 0.9 folks

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Pituophis',
+    version='0.9',
+    packages=['pituophis'],
+    url='https://github.com/dotcomboom/Pituophis',
+    license='BSD 2-Clause License',
+    author='dotcomboom',
+    author_email='dotcomboom@protonmail.com',
+    description='Python 3 library for building Gopher clients and servers'
+)
